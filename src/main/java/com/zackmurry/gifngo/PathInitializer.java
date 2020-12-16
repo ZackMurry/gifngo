56980c129d59bf81c6f7b83697ca432e72ea0c2c
Zack Murry <zackk.murryy@gmail.com>
2020-12-16 09:19:50 -0600

    Fix path updating (hopefully)
    It now works even after restarting your computer.

--- src/main/java/com/zackmurry/gifngo/PathInitializer.java
+++ src/main/java/com/zackmurry/gifngo/PathInitializer.java
@@ -3,17 +3,37 @@ package com.zackmurry.gifngo;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.net.URLDecoder;
 import java.nio.charset.StandardCharsets;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
+import java.util.function.Consumer;
 
 public class PathInitializer {
 
     private static final Logger logger = LoggerFactory.getLogger(PathInitializer.class);
 
     private static final String GIFNGO_BIN_PATH = System.getProperty("user.home") + "\\gifngo\\bin";
+
+    /**
+     * basically just takes response from a cmd command and prints it to the screen
+     */
+    private static class StreamGobbler implements Runnable {
+        private final InputStream inputStream;
+        private final Consumer<String> consumer;
+
+        public StreamGobbler(InputStream inputStream, Consumer<String> consumer) {
+            this.inputStream = inputStream;
+            this.consumer = consumer;
+        }
+
+        @Override
+        public void run() {
+            new BufferedReader(new InputStreamReader(inputStream)).lines()
+                    .forEach(consumer);
+        }
+    }
 
     public static void initialize() {
         try {
@@ -23,9 +43,16 @@ public class PathInitializer {
             String osName = System.getProperty("os.name");
             // todo support other OSs
             if (osName.startsWith("Windows")) {
-                Runtime.getRuntime().exec("cmd /c \"setx PATH \\\"%PATH%;" + GIFNGO_BIN_PATH + "\\\"\"");
+                // todo might want to check if path already has /gifngo/bin in it
+                final ProcessBuilder builder = new ProcessBuilder();
+                // todo might not need the semicolon here
+                builder.command("cmd.exe", "/c", "setx", "/M", "PATH", "\"%PATH%;" + GIFNGO_BIN_PATH + "\"");
+                final Process process = builder.start();
+                StreamGobbler gobbler = new StreamGobbler(process.getInputStream(), System.out::println);
+                gobbler.run();
+                logger.info("Initialization finished. Please restart your computer to see if it has succeeded. If it failed, make sure that you ran this program in the administrator command prompt.");
             } else {
-                logger.error("Operating system not supported for this command. You'll have to do update your PATH to include " + GIFNGO_BIN_PATH + " manually.");
+                logger.error("Operating system not supported for this command. You'll have to update your PATH to include " + GIFNGO_BIN_PATH + " manually.");
             }
 
 
@@ -47,17 +74,21 @@ public class PathInitializer {
         if (!gifngoBatchFile.createNewFile()) {
             logger.warn("The batch file for gifngo already exists. Replacing...");
         }
-        String path = PathInitializer.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+        final String path = PathInitializer.class.getProtectionDomain().getCodeSource().getLocation().getPath();
         String decodedPath = URLDecoder.decode(path, StandardCharsets.UTF_8);
         if (!decodedPath.endsWith(".jar")) {
             logger.error("To initialize, you should run directly from the jar file. Expected location to end with .jar but instead is: {}", decodedPath);
             return false;
         }
+        if (decodedPath.startsWith("/") || decodedPath.startsWith("\\")) {
+            decodedPath = decodedPath.substring(1);
+        }
         logger.debug("Path of .jar: {}", decodedPath);
-        FileWriter fw = new FileWriter(gifngoBatchFile);
+        final FileWriter fw = new FileWriter(gifngoBatchFile);
         // todo might want to change /k to /c
         fw.write("@ECHO OFF\n" +
                 "cmd /k \"java -jar " + decodedPath + " %*\"");
+        fw.close();
         return true;
     }
 
